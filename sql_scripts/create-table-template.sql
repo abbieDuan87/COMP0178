5de6ad1cab2fa5ee0d6c64419af5df44bfd20a40
minjnlgc <kunyuquemian@outlook.com>
2024-10-24 15:29:15 +0100

    Added the table template sql script

---
+++ sql_scripts/create-table-template.sql
@@ -0,0 +1,99 @@
+# Drop the database if exists
+DROP DATABASE IF EXISTS auction;
+CREATE DATABASE auction;
+USE auction;
+
+# Create the tables
+CREATE TABLE Users(  
+    userID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    dateOfBirth DATETIME
+) COMMENT '';
+
+
+CREATE TABLE Sellers(
+    sellerID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (sellerID) REFERENCES Users(userID)
+);
+
+CREATE TABLE Buyers(
+    buyerID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (buyerID) REFERENCES Users(userID)
+);
+
+CREATE TABLE Addresses(
+    addressID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userID INT NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    postcode VARCHAR(255) NOT NULL,
+    FOREIGN KEY (userID) REFERENCES Users(userID)
+);
+
+
+CREATE TABLE Categories (
+    categoryID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+INSERT INTO Categories (name)
+VALUES 
+    ('Motors'),
+    ('Fashion'),
+    ('Home Garden'),
+    ('Collectables & Art'),
+    ('Sports, Hobbies & Leisure'),
+    ('Electronics'),
+    ('Health & Beauty'),
+    ('Business, Office & Industrial Supplies'),
+    ('Media'),
+    ('Others');
+
+CREATE TABLE Auctions (
+    auctionID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, -- Fix: changed 'PRIMARY' to 'PRIMARY KEY'
+    categoryID INT NOT NULL,
+    sellerID INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255), -- You can decide if this should be NOT NULL
+    createdDate DATETIME NOT NULL, -- Use PHP to set the date, or set default CURRENT_TIMESTAMP
+    endDate DATETIME NOT NULL,
+    startingPrice FLOAT NOT NULL,
+    reservePrice FLOAT, -- Correct spelling from 'reversePrice' to 'reservePrice'
+    itemImage BINARY,
+    auctionStatus BOOLEAN NOT NULL,
+    itemCondition ENUM('new', 'good', 'used') NOT NULL,
+    FOREIGN KEY (categoryID) REFERENCES Categories(categoryID)
+);
+
+CREATE TABLE Bids(
+    bidID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    auctionID INT NOT NULL,
+    buyerID INT NOT NULL,
+    bidPrice FLOAT NOT NULL,
+    bidDate DATETIME NOT NULL,
+    isSuccessful BOOLEAN NOT NULL,
+    FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
+    FOREIGN KEY (buyerID) REFERENCES Buyers(buyerID)
+);
+
+CREATE TABLE Orders(
+    orderID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    auctionID INT NOT NULL,
+    buyerID INT NOT NULL,
+    sellerID INT NOT NULL,
+    orderDate DATETIME NOT NULL,
+    orderStatus ENUM('placed', 'dispatched', 'delivered'),
+    FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
+    FOREIGN KEY (buyerID) REFERENCES Buyers(buyerID),
+    FOREIGN KEY (sellerID) REFERENCES sellers(sellerID)
+);
+
+CREATE TABLE Watchlists(
+    buyerID INT NOT NULL,
+    auctionID INT NULL NULL,
+    FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
+    FOREIGN KEY (buyerID) REFERENCES Buyers(buyerID)
+);
