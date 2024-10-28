b0f0e5d8eea7aead6170546d21eb2aba144bbaf7
minjnlgc <kunyuquemian@outlook.com>
2024-10-28 17:27:02 +0000

    feat: implemented sort by category, price, and closed date, added execute_query, and close query helper function changed utilities print_listing_li, 'bid' should be singular when 1 or 0 bid implemented when no query result show 'no result' show the auctions in browse.php

--- database.php
+++ database.php
@@ -9,3 +9,18 @@ function get_connection()
 
     return $connection;
 }
+
+function execute_query($connection, $sql)
+{
+    $result = mysqli_query($connection, $sql);
+
+    if (!$result) {
+        die('Query error: ' . mysqli_error($connection));
+    }
+    return $result;
+}
+
+function close_connection($connection)
+{
+    mysqli_close($connection);
+}
