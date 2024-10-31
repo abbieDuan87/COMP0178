9fd85924b5743d5fff64094cc82fa59dc35ba094
abbieduan <abbiehduan@gmail.com>
2024-10-31 18:03:02 +0000

    added username attribute to database and removed dob

--- register.php
+++ register.php
@@ -57,6 +57,14 @@ if (isset($_SESSION['error_message'])) {
   </div>
 
   <div class="form-group row">
+    <label for="username" class="col-sm-2 col-form-label text-right">Username</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+      <small id="usernameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+    </div>
+  </div>
+
+  <div class="form-group row">
     <label for="password" class="col-sm-2 col-form-label text-right">Password</label>
     <div class="col-sm-10">
       <input type="password" class="form-control" id="password" name="password" placeholder="Password">
