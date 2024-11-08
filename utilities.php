4fc1831099629c7a8f55cfd5d166376f38bbc756
rheahemmadi <rheahemmadi@gmail.com>
2024-11-08 14:33:42 +0000

    consistent use of bid v bids

--- utilities.php
+++ utilities.php
@@ -69,7 +69,7 @@ function print_listing_li($item_id, $title, $currentPrice, $num_bids, $end_time)
 function print_my_bids_listing($item_id, $title, $highestBid, $userBid, $num_bids, $end_time)
 {
   // Fix language of bid vs. bids
-  if ($num_bids <= 1) {
+  if ($num_bids = 1) {
     $bid = ' bid';
   } else {
     $bid = ' bids';
